f6fa4e64c5aac7a0364b591741915e56f2127a17
Isabel11 <Isabel.rlpeters@gmail.com>
2013-07-21 20:55:04 +0200

    Small clean ups

--- api.py
+++ api.py
@@ -13,45 +13,53 @@ def products():
 
 
 	if request.method == 'GET':
-		since = request.args.get('since')
-		if(since != None):
-			db.execute("DELETE FROM items WHERE last_updated < '%s'"%getDateString(since))
+		if(request.args.get('since') != None):
+			db.execute("DELETE FROM items WHERE last_updated < '%s'"%__getDateString(since))
 
 		dbrequest = db.select('SELECT * FROM items')
-		
+		columns = db.select('PRAGMA table_info(items)')
+		dicts = []
 
+		for item in dbrequest:
+			item_dict = {}
+			for i, col in enumerate(columns):
+				item_dict[col[1]] = item[i]
+			dicts.append(item_dict)		
+		return  json.dumps({'result:':dicts})
+	
+	elif request.method == 'PUT':
+		body = request.data
+		jsonBody = json.loads(body)
+		exists = __rowExists(jsonBody['code'], jsonBody['raw_color'])
+		if(exists):
+			__updateItem(jsonBody)
+			return  json.dumps({'result:':'updated'})
+		else: 
+			__insertItem(jsonBody)
+			return json.dumps({'result:':'added'})
+
+
+@app.route('/rest/products/render')
+def render():
+	""" I used this for testing purposes and to analyse if output data was correct """
+
+	error = None
+	if request.method == 'GET':
+		if(request.args.get('since') != None):
+			db.execute("DELETE FROM items WHERE last_updated < '%s'"%__getDateString(since))
+
+		dbrequest = db.select('SELECT * FROM items')
 		columns = db.select('PRAGMA table_info(items)')
 		dicts = []
 		for item in dbrequest:
 			item_dict = {}
 			for i, col in enumerate(columns):
 				item_dict[col[1]] = item[i]
-			dicts.append(item_dict)						
-		jsonDumps = json.dumps({'result:':dicts})
-		return jsonDumps
-	
-	elif request.method == 'PUT':
-		body = request.data
-		jsonBody = json.loads(body)
-		exists = rowExists(jsonBody['code'], jsonBody['raw_color'])
-		print exists 
-		if(exists):
-			updateItem(jsonBody)
-			jsonDumps = json.dumps({'result:':'updated'})
-			return jsonDumps
-		else: 
-			insertItem(jsonBody)
-			jsonDumps = json.dumps({'result:':'added'})
-			return jsonDumps
+			dicts.append(item_dict)					
+		return render_template('lystTemplateReduced.html', items = dicts)		
 		
-		
-def rowExists(code, raw_color):
-	""" tests if a row with unique identifier exists """
-	
-	items = db.select("SELECT EXISTS(SELECT * FROM items WHERE code = '%s' AND raw_color = '%s')"% (code, raw_color))
-	return items[0][0]
 
-def insertItem(jsonBody):
+def __insertItem(jsonBody):
 	""" calls the database and inserts the values"""
 
 	image_urls = json.dumps(jsonBody['image_urls'])
@@ -73,35 +81,21 @@ def insertItem(jsonBody):
 			 jsonBody['type'],
 			 ])
 
-def updateItem(jsonBody):
+def __rowExists(code, raw_color):
+	""" tests if a row with unique identifier exists """
+	
+	items = db.select("SELECT EXISTS(SELECT * FROM items WHERE code = '%s' AND raw_color = '%s')"% (code, raw_color))
+	return items[0][0]
+
+
+def __updateItem(jsonBody):
 	""" updates an item in the database"""
 
 	update = "UPDATE items SET last_updated = '%s' WHERE code = '%s' AND raw_color = '%s' "% ( jsonBody['last_updated'],  jsonBody['code'],  jsonBody['raw_color'])
 	db.execute(update)
-	
-@app.route('/rest/products/render')
-def render():
-	""" I used this for testing purposes and to analyse if output data was correct """
 
-	error = None
-	if request.method == 'GET':
-		since = request.args.get('since')
-		if(since != None):
-			db.execute("DELETE FROM items WHERE last_updated < '%s'"%getDateString(since))
 
-		dbrequest = db.select('SELECT * FROM items')
-		
-
-		columns = db.select('PRAGMA table_info(items)')
-		dicts = []
-		for item in dbrequest:
-			item_dict = {}
-			for i, col in enumerate(columns):
-				item_dict[col[1]] = item[i]
-			dicts.append(item_dict)					
-		return render_template('lystTemplateReduced.html', items = dicts)
-
-def getDateString(parameter):
+def __getDateString(parameter):
 	""" converts the parameter from the URL into a string that can be used for a databaseRequest """
 	
 	formatted = datetime.datetime.strptime(parameter, "%d-%m-%Y")
